558b75471b6e76b17450a6315e9af61c3a4e9747
Jimmy Alford <jimmy.alford316@gmail.com>
2024-07-28 13:59:57 -0400

    rename score to scores and give it a better default

--- priv/repo/migrations/20240716141626_add_players_table.exs
+++ priv/repo/migrations/20240716141626_add_players_table.exs
@@ -4,7 +4,7 @@ defmodule Drafter.Repo.Migrations.AddPlayersTable do
   def change do
     create table("players") do
       add :name, :string, null: false
-      add :score, {:array, :integer}, default: []
+      add :scores, {:array, :integer}, default: [0, 0, 0, 0]
       add :tournament_id, references(:tournaments), null: false
       add :user_id, references(:users)
       
